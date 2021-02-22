7b80fc52cf9ba7db2b17566afcff5a2acf1c5688
Tw <wei.tan@intel.com>
2021-02-22 10:43:40 +0800

    back to empty
    Signed-off-by: Tw <wei.tan@intel.com>

--- prompt/doc.go
+++
@@ -1,8 +0,0 @@
-/*
-package prompt implements several prompt windows that can be used in any X
-window manager. They are rather crude as they are built using X without any
-GUI toolkits.
-
-See the examples directory for how to use each of the prompts.
-*/
-package prompt
